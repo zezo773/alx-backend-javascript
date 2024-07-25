07dbe97d52a1bc176faa5fedfcb94f2f09a16174
zezo773 <zezo77328@gmail.com>
2024-07-25 11:06:20 +0300

    afas

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = array;
-  for (let value of array) {
-    let idx = array.indexOf(value);
+  const newArray = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
     newArray[idx] = appendString + value;
   }
 
